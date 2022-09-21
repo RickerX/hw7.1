11498df403345ec69dc39c0de0a1404da352dfa2
roman <roman.pshenichnikov555@gmail.com>
2022-09-21 17:34:49 +0300

    hw7.1 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача 1
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника — "+fullName);
+        System.out.println("ФИО сотрудника — "+lastName + firstName + middleName);
+        // Задача 2
+        fullName = fullName.replace("Ivanov Ivan Ivanovich", "IVANOV IVAN IVANIVICH");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName);
+        // Задача 3
+        String fullNameTask3 = "Иванов Семён Семёнович";
+        fullNameTask3 = fullNameTask3.replace("ё", "е");
+        System.out.println("ФИО сотрудника — "+fullNameTask3);
+
     }
 }
